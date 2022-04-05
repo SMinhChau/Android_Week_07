efa52c7d666e0e9da9513aef089a2233cd4e2dc9
minhchau <minhchaunguyenmtp@gmail.com>
2022-04-05 07:20:13 +0700

    android_week_07

---
+++ app/src/main/java/com/example/android_week_07/Persion.java
@@ -0,0 +1,30 @@
+package com.example.android_week_07;
+
+public class Persion {
+    int id;
+    int name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public Persion(int id, int name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Persion() {
+    }
+}
